4f91b1fc8bda8b70dedfd0a02fce3c5ea4c0578f
wighawag <wighawag@gmail.com>
2025-12-21 10:20:57 +0000

    bypass issue with earliest

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,25 @@ async function main() {
 	});
 	await client.tevmReady();
 
-	const provider = extendProviderWithAccounts(client as any, {
+	let provider = extendProviderWithAccounts(client as any, {
 		accounts: {
 			mnemonic: 'test test test test test test test test test test test junk',
 		},
 	});
+
+	provider = ((p) => {
+		return {
+			request(args: {method: string; params?: any[]}) {
+				if (args.method === 'eth_getBlockByNumber' && args.params?.[0] === 'earliest') {
+					return p.request({
+						method: 'eth_getBlockByNumber',
+						params: ['0x0', args?.params[1]] as any,
+					});
+				}
+				return p.request(args as any);
+			},
+		} as any;
+	})(provider);
 
 	const {loadAndExecuteDeploymentsFromModules} = setupEnvironment(config, extensions);
 	await loadAndExecuteDeploymentsFromModules([{id: 'DeployScript', module: DeployScript}], {
